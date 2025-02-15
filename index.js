45caccdeaf6fbafd5ae16c44e5031d63907247ee
Chandana-Shetty <144927546+chandanashetty31@users.noreply.github.com>
2025-02-15 22:33:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import bcrypt from "bcrypt";
 import bodyParser from "body-parser";
 import express from "express";
 import { dirname } from "path";
@@ -8,6 +9,7 @@ import { fileURLToPath } from "url";
 import dotenv from 'dotenv';
 dotenv.config();
 console.log(process.env);
+const saltRounds = parseInt(process.env.SALT_ROUNDS);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -23,22 +25,22 @@ db.connect();
 const app = express();
 const port = 3000;
 
-// Set EJS as the templating engine
+
 app.set("view engine", "ejs");
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public")); // Serve static files
+app.use(express.static("public")); 
 
-// Route to fetch book data
+
 app.get("/books", async (req, res) => {
   const books = [
-    "9788172234980", // ISBN for "The Alchemist"
-    "9781786330895", // ISBN for "Ikigai"
+    "9788172234980", // ISBN of "The Alchemist"
+    "9781786330895", // ISBN of "Ikigai"
   ];
 
   try {
-    // Fetch book data from Open Library
+    
     const bookPromises = books.map((isbn) =>
       axios.get(
         `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`
@@ -47,7 +49,7 @@ app.get("/books", async (req, res) => {
 
     const responses = await Promise.all(bookPromises);
 
-    // Extract relevant book details
+    
     const booksData = responses.map((response, index) => {
       const bookKey = `ISBN:${books[index]}`;
       const book = response.data[bookKey];
@@ -67,7 +69,7 @@ app.get("/books", async (req, res) => {
     const checkBook = await db.query("SELECT * FROM book WHERE isbn = $1", [book.isbn]);
 
     if (checkBook.rows.length === 0) {
-      // Insert into the database
+      
       await db.query("INSERT INTO book (title, author, isbn) VALUES ($1, $2, $3)", [book.title, book.authors, book.isbn]);
     }
 
@@ -79,7 +81,6 @@ app.get("/books", async (req, res) => {
   res.status(500).send("Error fetching books");
 }
     
-    // Render index.ejs and pass the booksData
     res.render("index", { books: booksData ,
       title:booksData[0].title,
       author:booksData[0].authors,
@@ -111,7 +112,7 @@ app.get("/login",(req, res) => {
 app.post("/logedin",async(req, res) => {
   console.log(req.body);
   const email = req.body.email;
-  const password = req.body.password;
+  const userpassword = req.body.password;
 
   try {
     const result = await db.query("SELECT * FROM userdata WHERE email = $1", [
@@ -119,13 +120,19 @@ app.post("/logedin",async(req, res) => {
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      const storedPassword = user.password;
+      const storedHashPassword = user.password;
 
-      if (password === storedPassword) {
-        res.redirect("/books");
-      } else {
-        res.send("Incorrect Password");
-      }
+     bcrypt.compare(userpassword, storedHashPassword, (err, result) => {
+        if (err) {
+          console.error("Error comparing passwords:", err);
+        } else {
+          if (result) {
+            res.redirect("/books");
+          } else {
+            res.send("Incorrect Password");
+          }
+        }
+      });
     } else {
       res.send("User not found");
     }
@@ -146,19 +153,25 @@ app.post("/signedup",async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-      const result = await db.query(
-        "INSERT INTO userdata (name ,email, password) VALUES ($1, $2, $3)",
-        [name,email, password]
-      );
-      console.log(result);
-      res.redirect("/login");
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          console.log("Hashed Password:", hash);
+          const result =await db.query(
+           "INSERT INTO userdata (name ,email, password) VALUES ($1, $2, $3)",
+        [name,email, hash]
+          );
+          res.redirect("/login");
+        }
+      });
     }
   } catch (err) {
     console.log(err);
   }
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
